69dc1e5793e9cdfdc3a9b88b3b0f0f8940c1c2ce
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-17 20:14:48 +0100

    chore: add image to sign in option :zap:

--- app/(root)/auth/sign-option.tsx
+++ app/(root)/auth/sign-option.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRouter } from 'expo-router';
 import { SIGN_IN_URL, SIGN_UP_URL } from '@/config/routes';
+import images from '@/constants/images';
 
 export default function SignOption() {
   const router = useRouter();
@@ -10,8 +11,9 @@ export default function SignOption() {
   return (
     <SafeAreaView className="flex-1 bg-black">
       <View className="flex-1 px-5 justify-center">
+        <Image source={images.empresosIconWhite} className='w-32 h-32 mx-auto my-12' />
         <View className="mb-20">
-          <Text className="text-white text-4xl font-bold mb-16">YOU ARE{'\n'}WELCOME</Text>
+          {/* <Text className="text-white text-4xl font-bold mb-16">YOU ARE{'\n'}WELCOME</Text> */}
 
           <View className='flex flex-col items-center justify-center'>
             <Text className="text-white text-xl mb-2">Hi there!</Text>
