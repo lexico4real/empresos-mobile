bc6ba0d879981ee3b3933cad2b6a846f4d3159d7
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-17 20:09:04 +0100

    feat: config general routes :racehorse:

---
+++ config/routes.ts
@@ -0,0 +1,9 @@
+const SIGN_IN_URL = "/auth/sign-in";
+const SIGN_UP_URL = "/auth/sign-up";
+const SIGN_OPTION_URL = "/auth/sign-option";
+
+export {
+  SIGN_IN_URL,
+  SIGN_UP_URL,
+  SIGN_OPTION_URL,
+}
