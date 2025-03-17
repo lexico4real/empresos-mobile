d0ff802b5740e38595e8c4cfff78e3c1477e64bb
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-17 15:54:55 +0100

    chore: config splash screen :zap:

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,37 @@
 import { Stack } from "expo-router";
 import "./global.css";
+import { useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { View } from "react-native";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Add any initialization logic here
+        // For example, loading fonts, making API calls, etc.
+
+        // Artificially delay for 2 seconds to see the splash screen
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
+        setIsReady(true);
+        await SplashScreen.hideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
+
   return <Stack />;
 }
---
+++ constants/images.ts
@@ -0,0 +1,12 @@
+import onboarding from "@/assets/images/onboarding.png";
+import empresosIconWhite from "@/assets/images/empresos-icon-white.png";
+import empresosIconRed from "@/assets/images/empresos-icon-red.png";
+
+
+
+
+export default {
+  onboarding,
+  empresosIconWhite,
+  empresosIconRed,
+};
