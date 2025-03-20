1e0e7527e4086647ed9f9f9b88380f09f44736f1
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-20 08:20:10 +0100

    feat: include types :fire:

--- config/types.ts
+++ config/types.ts
@@ -1,7 +1,27 @@
-export interface SlideItem {
+import { ImageSourcePropType } from "react-native";
+
+interface SlideItem {
   id: number;
   image: any;
   title: string;
   titleHighlight: string;
   subtitle: string;
 }
+
+
+interface ProfileHeaderProps {
+  title: string
+  showBackArrow?: boolean
+  showNotification?: boolean
+  onBackPress?: () => void
+  onNotificationPress?: () => void
+  rightComponent?: React.ReactNode
+  backArrowIcon?: ImageSourcePropType
+  logo?: ImageSourcePropType
+  rightIcon?: ImageSourcePropType
+  onRightPress?: () => void
+}
+
+
+
+export type { SlideItem, ProfileHeaderProps }
