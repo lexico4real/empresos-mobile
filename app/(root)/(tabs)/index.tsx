8faa1ce9a2e3343a0b1974e088bc8a04d45bfad1
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-20 08:18:35 +0100

    chore: root tab :zap:

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,11 +1,22 @@
+import Header from '@/components/common/header'
+import icons from '@/constants/icons'
 import React from 'react'
-import { Text } from 'react-native'
+import { ScrollView, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Index() {
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView className='flex-1 bg-white'>
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerClassName='pb-32 '>
+        <Header
+          title="Empresos"
+          showBackArrow={true}
+          backArrowIcon={icons.back}
+          logo={icons.logo}
+          rightIcon={icons.close}
+          onRightPress={() => console.log('Close pressed')}
+        />
+      </ScrollView>
     </SafeAreaView>
   )
 }
