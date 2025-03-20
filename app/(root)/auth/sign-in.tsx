aab8a8d4a4bc548155ec6de36ae67f55ab5015a0
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-20 08:19:07 +0100

    chore: sign in page :zap:

--- app/(root)/auth/sign-in.tsx
+++ app/(root)/auth/sign-in.tsx
@@ -1,3 +1,6 @@
+import Button from '@/components/common/button'
+import { HOME } from '@/constants/routes'
+import { router } from 'expo-router'
 import React from 'react'
 import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -6,6 +9,11 @@ export default function SignIn() {
   return (
     <SafeAreaView>
       <Text>SignIn</Text>
+      <Button onPress={() => {
+        router.push(HOME)
+      }}>
+        <Text>Go Home</Text>
+      </Button>
     </SafeAreaView>
   )
 }
