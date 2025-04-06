9b4b0351fb8a51ae341cd1f2068e41983e559043
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-04-06 17:33:23 +0100

    chore: include new route :zap:

--- config/routes.ts
+++ config/routes.ts
@@ -1,9 +1,11 @@
 const SIGN_IN_URL = "/auth/sign-in";
 const SIGN_UP_URL = "/auth/sign-up";
 const SIGN_OPTION_URL = "/auth/sign-option";
+const HOME_URL = "/(root)/(tabs)";
 
 export {
   SIGN_IN_URL,
   SIGN_UP_URL,
   SIGN_OPTION_URL,
+  HOME_URL,
 }
